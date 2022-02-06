2f30007c46e6d1e4ca06ab001f0e74e3ddb15ff6
Alwi-Adrian <99113057+Alwi-Adrian@users.noreply.github.com>
2022-02-06 22:14:05 +0700

    Add files via upload

---
+++ tugas alwi adrian.py
@@ -0,0 +1,8 @@
+print("Nama : Alwi adrian")
+print("Npm  : G1A021079")
+print(   "Tugas SDA")
+
+r = int(input("masukan jari_jari lingkaran: "))
+phi = 3.14
+L = phi*r*r
+print("luas lingkaran dengan jari-jari {} adalah {}".format(r,L))
